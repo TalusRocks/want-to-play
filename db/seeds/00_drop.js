d0a9ac70ce7f8b3d5c9343eff648fe9391244887
Wes Reid <bwreid@gmail.com>
2017-12-05 10:35:14 -0800

    Update 00_drop.js

--- db/seeds/00_drop.js
+++ db/seeds/00_drop.js
@@ -2,11 +2,8 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('games_tags').del()
-    .then(() => {
-      return knex('tags').del()
-    })
-    .then(() => {
-      return knex('games').del()
-    })
+    // remember that these can be shortened if you're returning
+    .then(() => knex('tags').del())
+    .then(() => knex('games').del())
 
 }
